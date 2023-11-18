a367006c2c9d9467656114c4e76decc74fcfe5c1
vovanky99 <150527055+vovanky99@users.noreply.github.com>
2023-11-18 09:43:44 +0700

    Dựng khung Layout mặc định

--- src/components/layout/defaultLayout/Sidebar/index.js
+++ src/components/layout/defaultLayout/Sidebar/index.js
@@ -1,4 +1,13 @@
+import styles from './Sidebar.module.scss';
+import classNames from 'classnames/bind';
+
+const cx = classNames.bind(styles);
+
 function Sidebar() {
-    return <h1>Sidebar</h1>;
+    return (
+        <div className={cx('wrapper')}>
+            <h1> sidebar </h1>
+        </div>
+    );
 }
 export default Sidebar;
