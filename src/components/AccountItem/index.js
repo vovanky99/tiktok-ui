638300ea0f3f8f7c8846a69ce4611afd9e8c53ad
vovanky99 <150527055+vovanky99@users.noreply.github.com>
2023-11-19 20:32:38 +0700

    Xây dựng UI phần Header #2

---
+++ src/components/AccountItem/index.js
@@ -0,0 +1,27 @@
+import classNames from 'classnames/bind';
+import Style from './AccountItem.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+
+const cx = classNames.bind(Style);
+
+function AccountItem() {
+    return (
+        <div className={cx('wrapper')}>
+            <img
+                className={cx('avatar')}
+                src="https://p16-sign-useast2a.tiktokcdn.com/tos-useast2a-avt-0068-aiso/6e34532fc12701d8ecbe4ad90ea4682c~c5_300x300.webp?x-expires=1700571600&x-signature=B8ScIfnX5g28yL3mf4Ho17tUYrg%3D"
+                alt="aaa"
+            />
+            <div className={cx('info')}>
+                <h4 className={cx('name')}>
+                    <span>nguyen Van a</span>
+                    <FontAwesomeIcon className={cx('check-icon')} icon={faCheckCircle} />
+                </h4>
+                <span className={cx('username')}>nguyenvana</span>
+            </div>
+        </div>
+    );
+}
+
+export default AccountItem;
